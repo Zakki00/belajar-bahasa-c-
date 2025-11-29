90591f7f0f2f269f144b6d38ac7e2aaa3e9c034c
Zakki00 <zakkimubarroq06@gmail.com>
2025-11-29 09:21:10 +0700

    belajar faktorial dengan for

---
+++ oprator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+void operator_aritmatika(){
+    int a = 10;
+    int b = 3;
+
+    cout << "Penjumlahan: " << (a + b) << endl;
+    cout << "Pengurangan: " << (a - b) << endl;
+    cout << "Perkalian: " << (a * b) << endl;
+    cout << "Pembagian: " << (a / b) << endl;
+    cout << "Modulus: " << (a % b) << endl;
+}
+
+
+int main(){
+    int i = 1;
+    i++;
+    cout << i << endl; // Output: 2
+
+}
